6e4fe313b23106fe2331b1db23686fb6416533e6
Elmerrrrrr <volgers80@gmail.com>
2021-01-05 15:15:00 +0100

    small update

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -5,10 +5,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src="https://image.tmdb.org/t/p/original/wwemzKWzjKYJFfCeiB57q3r4Bcm.svg"  className="App-logo1" width="50%" alt="logo" />
-        <p>
-         Netflix clone - Final Project
-         Mari, Sunny, Kacem, Jouke and Elmer
-        </p>
+        <p>Netflix clone - Final Project</p>
+        <p>By Mari, Sunny, Kacem, Jouke and Elmer</p>
+        
         <a
           className="App-link"
           href="https://reactjs.org"
